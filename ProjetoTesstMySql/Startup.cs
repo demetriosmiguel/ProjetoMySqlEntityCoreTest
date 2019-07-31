bae4b611973c027318eebac2266738ee31357899
Demetrios Miguel <demetrios.miguel@qwti.com.br>
2019-07-31 14:23:39 -0300

    Teste

--- ProjetoTesstMySql/Startup.cs
+++ ProjetoTesstMySql/Startup.cs
@@ -31,6 +31,7 @@ namespace ProjetoTesstMySql
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                // Teste
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
